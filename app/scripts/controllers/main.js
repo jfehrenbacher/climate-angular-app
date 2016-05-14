7362c1e2ac7f72ee475f027934bcfc74f29222c4
jfehrenbacher <fehrenb1@seattleu.edu>
2016-05-13 18:27:31 -0700

    Bootstrapped project

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name climateAngularAppApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the climateAngularAppApp
+ */
+angular.module('climateAngularAppApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
